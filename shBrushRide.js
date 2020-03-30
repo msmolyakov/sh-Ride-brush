c58adb514fd1d229f7446692e6da951533fe661f
Maxim Smolyakov <smolyakovmaxim@gmail.com>
2020-03-30 19:59:36 +0300

    support Ride V4

--- shBrushRide.js
+++ shBrushRide.js
@@ -1,22 +1,48 @@
 SyntaxHighlighter.brushes.Custom = function()
 {
-  var funcs       = 'extract value isDefined throw toBase58String fromBase58String parseInt log valueOrErrorMessage blake2b256'
-                    + ' getBinary getBoolean getInteger getString getBinaryValue getBooleanValue getIntegerValue getStringValue'
-                    + ' getElement fraction parseInt parseIntValue keccak256 size addressFromPublicKey takeRight take dropRight drop'
-                    + ' toBase64String fromBase64String wavesBalance assetBalance indexOf lastIndexOf toBase16String fromBase16String'
-                    + ' assetInfo sigVerify checkMerkleProof cons addressFromRecipient transactionHeightById sha256 pow blockInfoByHeight'
-                    + ' toInt toBytes split addressFromStringValue toString toUtf8String rsaVerify addressFromString transferTransactionById';
-​
-  var keywords    = 'func let if then else match case'
-                    + ' CEILING FLOOR DOWN HALFDOWN HALFEVEN HALFUP UP NOALG MD5 SHA1 SHA224 SHA256 SHA384 SHA512 SHA3224 SHA3256 SHA3384 SHA3512'
-                    + ' this unit nil height lastBlock Buy Sell';
-  var types       = 'Binary Boolean Int String List ScriptResult WriteSet TransferSet DataEntry ScriptTransfer Transfer'
-                    + ' Alias AssetPair Address AttachedPayment Asset Invocation BlockInfo Order Unit'
+  var keywords    = 'case else func if let match nil then';
+
+  var vars        = 'height lastBlock this tx unit Buy Sell'
+                    + 'CEILING FLOOR DOWN HALFDOWN HALFEVEN HALFUP UP NOALG MD5 SHA1 SHA224 SHA256 SHA384 SHA512 SHA3224 SHA3256 SHA3384 SHA3512';
+
+  var typesV2     = 'Address Alias AssetPair Binary Boolean BurnTransaction CreateAliasTransaction DataTransaction'
+                    + ' ExchangeTransaction GenesisTransaction Int IssueTransaction LeaseCancelTransaction LeaseTransaction'
+                    + ' MassTransferTransaction Order PaymentTransaction ReissueTransaction'
+                    + ' SetAssetScriptTransaction SetScriptTransaction SponsorFeeTransaction String'
+                    + ' Transaction Transfer TransferTransaction Unit';
+
+  var typesV3     = 'Asset AttachedPayment BlockInfo DataEntry Invocation InvokeScriptTransaction'
+                    + ' List ScriptResult ScriptTransfer TransferSet WriteSet'
                     + ' NoAlg Md5 Sha1 Sha224 Sha256 Sha384 Sha512 Sha3224 Sha3256 Sha3384 Sha3512'
-                    + ' Ceiling Down Floor HalfDown HalfEven HalfUp Up'
-                    + ' Transaction GenesisTransaction BurnTransaction InvokeScriptTransaction ReissueTransaction SetScriptTransaction'
-                    + ' LeaseTransaction LeaseCancelTransaction CreateAliasTransaction ExchangeTransaction MassTransferTransaction'
-                    + ' DataTransaction TransferTransaction IssueTransaction PaymentTransaction SponsorFeeTransaction SetAssetScriptTransaction';
+                    + ' Ceiling Down Floor HalfDown HalfEven HalfUp Up';
+
+  var typesV4     = 'BinaryEntry BooleanEntry Burn DeleteEntry IntegerEntry Issue Reissue StringEntry UpdateAssetInfoTransaction';
+
+  var types       = typesV2 + ' ' + typesV3 + ' ' + typesV4;
+
+  var funcsV2     = 'addressFromPublicKey addressFromRecipient addressFromStringValue assetBalance'
+                    + ' blake2b256 cons drop dropRight extract fraction fromBase58String fromBase64String'
+                    + ' getBinary getBinaryValue getBoolean getBooleanValue getElement getInteger getIntegerValue getString getStringValue'
+                    + ' isDefined keccak256 sha256 sigVerify size take takeRight throw toBase58String toBase64String toBytes toInt toString'
+                    + ' transactionHeightById wavesBalance';
+
+  var funcsV3     = 'assetInfo blockInfoByHeight checkMerkleProof fromBase16String indexOf lastIndexOf log'
+                    + ' parseInt parseIntValue pow rsaVerify split toBase16String toInt toUtf8String transferTransactionById'
+                    + ' value valueOrErrorMessage';
+
+  var blakeL      = 'blake2b256_16Kb blake2b256_32Kb blake2b256_64Kb blake2b256_128Kb';
+  var keccakL     = 'keccak256_16Kb keccak256_32Kb keccak256_64Kb keccak256_128Kb';
+  var shaL        = 'sha256_16Kb sha256_32Kb sha256_64Kb sha256_128Kb';
+  var sigVerifyL  = 'sigVerify_16Kb sigVerify_32Kb sigVerify_64Kb sigVerify_128Kb';
+  var rsaVerifyL  = 'rsaVerify_16Kb rsaVerify_32Kb rsaVerify_64Kb rsaVerify_128Kb';
+  var grothL      = 'groth16Verify_1inputs groth16Verify_2inputs groth16Verify_3inputs groth16Verify_4inputs groth16Verify_5inputs'
+                    + ' groth16Verify_6inputs groth16Verify_7inputs groth16Verify_8inputs groth16Verify_9inputs groth16Verify_10inputs'
+                    + ' groth16Verify_11inputs groth16Verify_12inputs groth16Verify_13inputs groth16Verify_14inputs groth16Verify_15inputs';
+
+  var funcsV4     = 'calculateAssetId contains createMerkleRoot groth16Verify median transferTransactionFromProtoBytes valueOrElse'
+                    + ' ' + blakeL + ' ' + keccakL + ' ' + shaL + ' ' + sigVerifyL + ' ' + rsaVerifyL + ' ' + grothL;
+
+  var funcs       = funcsV2 + ' ' + funcsV3 + ' ' + funcsV4;
  
   SyntaxHighlighter.regexLib.directives = /\{-#\s*(STDLIB_VERSION|CONTENT_TYPE|SCRIPT_TYPE)\s+\S+\s*#-\}/gm
   SyntaxHighlighter.regexLib.annotations = /@(Callable|Verifier)/gm
@@ -30,6 +56,7 @@ SyntaxHighlighter.brushes.Custom = function()
       { regex: SyntaxHighlighter.regexLib.strings,           css: 'string' },
       { regex: SyntaxHighlighter.regexLib.byteVectors,       css: 'color3' },
       { regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword' },
+      { regex: new RegExp(this.getKeywords(vars), 'gm'),     css: 'keyword' },
       { regex: new RegExp(this.getKeywords(types), 'gm'),    css: 'color1' },
       { regex: new RegExp(this.getKeywords(funcs), 'gm'),    css: 'color2' }
       ];
